7940270d4d7487342d59ae1926e7a669bfc2f171
apoorvakca <apoorvakulkarni.ca@gmail.com>
2021-09-13 08:09:29 -0400

    7 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatButtonModule, MatBadgeModule, MatIconModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
